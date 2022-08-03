437d6fa13864e3ae45a665b261c48adbea173751
deomorxsy <32879431+deomorxsy@users.noreply.github.com>
2022-08-03 20:17:13 -0300

    edit main class of webServer module

--- app/src/main/java/app/webServer.java
+++ app/src/main/java/app/webServer.java
@@ -1,6 +1,13 @@
-import java.net.*;
+import java.net.Socket;
 import java.io.*;
 
 public class webServer {
-	public static void main( )
+	public static void main(String[] args) {
+        ServerSocket server = new ServerSocket(80);
+        try {
+            System.out.println("Startando in 127.0.0.1:80");
+            Socket client = server.accept();
+            System.out.println("Startado: ggwp");
+        }
+    }
 }
