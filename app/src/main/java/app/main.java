21e35bd59efc3389f50277b135584f4b2edbce3f
deomorxsy <32879431+deomorxsy@users.noreply.github.com>
2022-08-04 21:26:05 -0300

    fix syntax errors check build process

--- app/src/main/java/app/main.java
+++ app/src/main/java/app/main.java
@@ -3,15 +3,15 @@ import java.io.*;
 import java.net.*;
 
 public class main {
-	public void static main(String[] args) {
+	public static void main(String[] args) {
     	try {
         	ServerSocket foo = new ServerSocket(4040);
 			Socket bar = foo.accept(); // connects
 DataInputStream getter = new DataInputStream(bar.getInputStream());
 
-String foobar = (String) getter.readUTF();
+            String foobar = (String) getter.readUTF();
 
-getter.close();
+            getter.close();
 		} catch(Exception e){System.out.println(e);}
 
 	}
--- app/src/main/java/app/webServer.java
+++ app/src/main/java/app/webServer.java
@@ -5,29 +5,27 @@ import java.io.*;
 public class webServer {
 	public static void main(String[] args) throws Exception {
         ServerSocket server = new ServerSocket(8080);
-        try {
-            System.out.println("Startando in 127.0.0.1:8080 ///\n Listening for connection...");
-            while (true ) {
-                // 0- instance of ServerSocket opens the socket.
-                /*Since HTTP is stateless, it does not need to store previous connections, so a loop is started, listening to a port, an integer passed as argument to the method, for any connection.
-                  Since
-                  */
-                Socket clientOnSocks = server.accept();
-                System.out.println("Startado: ggwp");
-                // 1- read HTTP request
-                // 2- generate response
-                // 3- sends back a HTTP response
-                // 4- close the socket
-                foo = clientOnSocks.getInputStream();
-                InputStreamReader bar = new InputStreamReader(foo);
-                BufferedReader foobar =  new BufferedReader(bar);
-                String waldo = foobar.readLine();
-                while (!(waldo.isEmpty)){
-                    System.out.println(waldo);
-                    waldo = foobar.readLine()
-                }
-
+        System.out.println("Startando in 127.0.0.1:8080 ///\n Listening for connection...");
+        while (true ) {
+            // 0- instance of ServerSocket opens the socket.
+            /*Since HTTP is stateless, it does not need to store previous connections, so a loop is started, listening to a port, an integer passed as argument to the method, for any connection.
+              Since
+              */
+            Socket clientOnSocks = server.accept();
+            System.out.println("Startado: ggwp");
+            // 1- read HTTP request
+            // 2- generate response
+            // 3- sends back a HTTP response
+            // 4- close the socket
+            //foo = clientOnSocks.getInputStream();
+            InputStreamReader bar = new InputStreamReader(clientOnSocks.getInputStream());
+            BufferedReader foobar =  new BufferedReader(bar);
+            String waldo = foobar.readLine();
+            while (!(waldo.isEmpty())){
+                System.out.println(waldo);
+                waldo = foobar.readLine();
             }
+
 
         }
     }
