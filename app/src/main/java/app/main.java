49cef18c6f1442f57be2d6912db7ed3dd03e02fc
Felipe-mth <ld17038@gmail.com>
2022-08-06 21:31:21 -0300

    protótipo de exibição

--- app/src/main/java/app/main.java
+++ app/src/main/java/app/main.java
@@ -12,6 +12,9 @@ public class Main {
 			ServerSocket foo = new ServerSocket(8080);
 			Socket bar = foo.accept(); // connects
 			DataInputStream getter = new DataInputStream(bar.getInputStream());
+			InputStreamReader bra = new InputStreamReader(bar.getInputStream());
+            BufferedReader br =  new BufferedReader(bra);
+		
 		
 
 			String foobar = (String) getter.readUTF();
@@ -19,9 +22,10 @@ public class Main {
 			Estudante e1 = new Estudante("12", "Lucas", "341325");
 			Professor p1 = new Professor("123", "felipe", "314981235", d1);
 			ConjuntoNotas cn1 = new ConjuntoNotas(e1, d1, 10, 4);		
-		
-			
-			System.out.println("teste");
+
+			String linha = br.readLine();
+			System.out.println(linha);
+			br.close();
 			getter.close();
 		} catch (Exception e) {
 			System.out.println(e);
--- app/src/main/java/app/webServer.java
+++ app/src/main/java/app/webServer.java
@@ -7,7 +7,7 @@ public class webServer {
 	public static void main(String[] args) {
        
         try {
-            ServerSocket server = new ServerSocket(8081);
+            ServerSocket server = new ServerSocket(8080);
             System.out.println("Startando in 127.0.0.1:80");
             Socket client = server.accept();
             System.out.println("Startado: ggwp");
