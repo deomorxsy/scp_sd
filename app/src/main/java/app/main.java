a7e9bed1978a055c65dc26a2e02ebce6a8b6db32
deomorxsy <32879431+deomorxsy@users.noreply.github.com>
2022-08-09 13:32:36 -0300

    tokenize header with URLconnection

--- app/src/main/java/app/main.java
+++ app/src/main/java/app/main.java
@@ -7,6 +7,8 @@ import java.net.Socket.*;
 import java.net.ServerSocket;
 import java.io.*;
 import java.util.concurrent.*;
+import java.net.URLConnection;
+
 
 public class main  {
 	public final static int PORT = 8080;
@@ -22,7 +24,7 @@ public class main  {
             pool.execute(new Runnable() {
 
                 public void run() { // it seems run can't throws IOException. For that, call() is used
-                    try {
+					try {
                         handleSocket(socket, numThreadsToSpawn);
                     } catch (IOException e) {
                         // Handle exception
@@ -32,18 +34,32 @@ public class main  {
 
                 }
 
-					public String handleSocket(final Socket socket, final int numThreadsToSpawn) throws IOException {
-						int currentThread = numThreadsToSpawn;
-						System.out.println("/// Startando FixedThread [currentThread/10]......EDIT");
+				public String handleSocket(final Socket socket, final int numThreadsToSpawn) throws IOException {
+					int currentThread = numThreadsToSpawn;
+
+
+                    String indexFileName = "index.html";
+					System.out.println("/// Startando FixedThread [currentThread/10]......EDIT");
                     // Do stuff with your socket, same as original code
 					//InputStream foo = socket.getInputStream();
                     //BufferedReader bar =  new BufferedReader(new InputStreamReader(foo, "UTF-8"));
 
                     //in
 
-					OutputStream raw = new BufferedOutputStream(socket.getOutputStream());
-					 Writer out = new OutputStreamWriter(raw);
-										BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); //works
+				   // OutputStream raw = new BufferedOutputStream(socket.getOutputStream());
+
+
+
+					Writer out = new BufferedWriter(
+						new OutputStreamWriter(socket.getOutputStream(), "US-ASCII"
+						)
+					 ); // OK
+
+                    //Writer out = new OutputStreamWriter(raw);
+
+                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); //works
+
+					// PRINT REQUEST HEADER
                     String foobar = in.readLine();
                     while (!(foobar.isEmpty())){
                         System.out.println(foobar);
@@ -53,16 +69,29 @@ public class main  {
                    // Writer out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "testando output"));
                     //return null;
 
+                     // Gets first line of header
+					 StringBuilder requestLine = new StringBuilder();
+					 while (true) {
+					     int c = in.read();
+					     if (c == '\r' || c == '\n') break;
+					    requestLine.append((char) c);
+					     }
 
-					 //StringBuilder requestLine = new StringBuilder();
-					 //while (true) {
-					 //    int c = in.read();
-					 //    if (c == '\r' || c == '\n') break;
-					 //   requestLine.append((char) c);
-					 //    }
+                    //// from StringBuilder to String
+                    String get = requestLine.toString();
+					String[] tokens = get.split("\\s+"); // splits every space found in the string
+					String httpVerb = tokens[0]; //http header method
+					String httpVersion= ""; //http header version
 
-
-
+					if (httpVerb.equals("GET")) {
+						String fileName = tokens[1];
+						if (fileName.endsWith("/")) fileName += indexFileName;
+						String contentKind =
+							URLConnection.getFileNameMap().getContentTypeFor(fileName);
+						if (tokens.length > 2) {
+							httpVersion = tokens[2];
+						}
+					}
 					String body = new StringBuilder("<HTML>\r\n")
 					 .append("<HEAD><TITLE>File Not Found</TITLE>\r\n")
 					 .append("</HEAD>\r\n")
